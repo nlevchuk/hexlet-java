cbd1bbc160f97824f83986d262fc05df003007dc
Nikita Levchuk <code@levch.uk>
2024-01-10 23:11:59 +0100

    add class with a 3rd party dependency

---
+++ io/hexlet/Example.java
@@ -0,0 +1,10 @@
+package io.hexlet;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Example {
+  public static void main(String[] args) {
+    var capitalizedWord = StringUtils.capitalize("hexlet");
+    System.out.println(capitalizedWord);
+  }
+}
